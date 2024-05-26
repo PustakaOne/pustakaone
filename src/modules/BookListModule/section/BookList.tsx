bca727133ca3bafcd9d873410eef8685bb622279
daffarafip24 <daffarafip24@gmail.com>
2024-05-26 19:53:22 +0700

    remove rating

--- src/modules/BookListModule/section/BookList.tsx
+++ src/modules/BookListModule/section/BookList.tsx
@@ -4,24 +4,22 @@ import { BOOKS } from '../constant';
 import Link from 'next/link';
 
 export const BookList: React.FC = () => {
-    return (
+  return (
     <section className="py-14">
-        <div className="container flex flex-col items-left gap-8">
-            <div className="flex flex-wrap justify-left gap-5">
-                {BOOKS.map((book, index) => (
-                  <Link href={`./${book.bookId.toString()}`} key={index}>
-                    <BookCard
-                    title={book.title}
-                    author={book.author}
-                    price={book.price}
-                    rating={book.rating}
-                    imageUrl={book.imageUrl}
-                    />
-                  </Link>
-                ))}
-            </div>
+      <div className="container flex flex-col items-left gap-8">
+        <div className="flex flex-wrap justify-left gap-5">
+          {BOOKS.map((book, index) => (
+            <Link href={`./${book.bookId.toString()}`} key={index}>
+              <BookCard
+                title={book.title}
+                author={book.author}
+                price={book.price}
+                imageUrl={book.imageUrl}
+              />
+            </Link>
+          ))}
         </div>
+      </div>
     </section>
-    );
+  );
 };
-    
