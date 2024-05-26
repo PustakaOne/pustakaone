397ce698354d6fc887bd1f703def58e4453f4cb9
steven-fo <stevenfo0711@gmail.com>
2024-05-26 23:55:45 +0700

    feat: integrate back end to front end in book detail

--- src/modules/BookDetailModule/index.tsx
+++ src/modules/BookDetailModule/index.tsx
@@ -1,12 +1,26 @@
+'use client';
+
 import React from 'react';
 import { BookBuy, BookDetail } from './section';
+import { useParams } from 'next/navigation';
+import { BookProps, BookDetailProps } from './interface';
 
-export const BookDetailModule: React.FC = () => {
+export const BookDetailModule: React.FC = async () => {
+  const params = useParams<{id: string}>();
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BOOKSHOP_URL}/books/${params.id}`,
+    {
+      cache: 'no-cache',
+    },
+  );
+
+  const book: BookDetailProps = await response.json();
+
   return (
     <>
       <div className='content flex flex-wrap py-5'>
         <div className='w-1/3'>
-          <BookBuy />
+          <BookBuy book={book}/>
         </div>
         <div className='w-2/3'>
           <BookDetail />
