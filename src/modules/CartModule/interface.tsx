9c2bda3bf40d52e825c42e74237ed77acbc8a12d
daffarafip24 <daffarafip24@gmail.com>
2024-05-26 21:44:51 +0700

    feat(cart): Cart page

--- src/modules/CartModule/interface.tsx
+++ src/modules/CartModule/interface.tsx
@@ -1,3 +1,35 @@
 export interface RemoveMeSectionProps {
   message: string;
 }
+
+export interface BookProps {
+  id: number;
+  lastId: number;
+  userId: number;
+  totalPrice: number;
+  paymentSuccess: boolean;
+  status: string;
+  bookCarts: BookCartProps[];
+  address: string;
+  paidAt: Date;
+}
+
+export interface BookCartProps {
+  id: number;
+  book: {
+    bookId: number;
+    title: string;
+    author: string;
+    publisher: string;
+    description: string;
+    price: number;
+    stock: number;
+    releaseDate: Date;
+    isbn: string;
+    coverUrl: string;
+    category: string;
+    pages: number;
+    lang: string;
+  };
+  amount: number;
+}
